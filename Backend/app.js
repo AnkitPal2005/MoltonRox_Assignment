222ccfe60dc9fdaa0fe73c8d4cba90c121857881
Ankitpal2005 <ap7023278@gmail.com>
2025-12-01 19:13:19 +0530

    Simplify CORS

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,7 @@ const express=require('express');
 const cors=require('cors');
 
 const app=express();
-app.use(cors({
-  origin: '*',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 const products=[
